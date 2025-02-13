032e822aabe9d389cb3b08ae71db4bb85ee8d40c
n0sat1y <sashamorozov533@gmail.com>
2025-02-13 15:36:02 +0300

    created database

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from router import router
+from database import create_tables, delete_tables
 
-app = FastAPI()
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+	await delete_tables()
+	print('Бд удалена')
+	await create_tables()
+	print('Бд создана')
+	yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router)
