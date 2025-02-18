141a8d0f0f11f68d295859ad390207e69ad7796e
n0sat1y <sashamorozov533@gmail.com>
2025-02-18 20:06:49 +0300

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from routers.tasks import router as task_router
 from routers.users import router as user_router
@@ -14,9 +15,22 @@ async def lifespan(app: FastAPI):
    await delete_tables()
    print("База очищена")
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost",
+]
+
 app = FastAPI(lifespan=lifespan)
 app.include_router(task_router)
 app.include_router(user_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == '__main__':
